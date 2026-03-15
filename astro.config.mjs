97d1159523942ed58dc67d7a9ce48f79e9f4013a
diligent2568 <265278864+diligent2568@users.noreply.github.com>
2026-03-15 19:00:40 -0400

    Use non-www site URL in astro.config
    Replace the site URL in astro.config.mjs from 'https://www.generalinterest.co' to 'https://generalinterest.co' to set the root domain as the canonical site URL. No other config changes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.generalinterest.co',
+	site: 'https://generalinterest.co',
 	integrations: [react(), sitemap()],
 });
